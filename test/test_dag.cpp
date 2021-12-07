9b3d7bd502c9351a5f0e03a54c3f2db3ae8bb491
Charles Nguyen <phucnh402@gmail.com>
2021-12-07 02:06:13 -0500

    Handle edge case
    - Notify user if no beer store/beer path is found
    - Notify user if the queried vertex is not found
    - Change prompt menu

--- test/test_dag.cpp
+++ test/test_dag.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include "test_dag.h"
 #include "../include/dag.h"
 #include "../include/graph/dcel.h"
+#include "../include/utils.h"
 
 
 
@@ -16,19 +17,20 @@ void test::test_dag(graph& G){
 	struct vertex* dest;
 	dag D;
 
-	cout << "\n\t--------------------Testing DAG Manual--------------------\n";
-	cout << "\t[1]	Build DAG\n";
-	cout << "\t[2]	Compute DAG Shortest Path\n";
-	cout << "\t[3]	Compute DAG Shortest Beer Path\n";
-	cout << "\t[4]	Query Time\n";
-		cout << "\t\t[1]	Time Building DAG\n";
-		cout << "\t\t[2]	Time Computing Shortest Path\n";
-		cout << "\t\t[3]	Time Computing Shortest Beer Path\n";
-		cout << "\t\t[4]	Time Getting Instance of Shortest Path\n";
-		cout << "\t\t[5]	Time Getting Instance of Shortest Beer Path\n\n";
 
 
 	while(1){
+		cout << "\t--------------------Testing DAG Manual--------------------\n";
+		cout << "\t[1]	Build DAG\n";
+		cout << "\t[2]	Compute DAG Shortest Path\n";
+		cout << "\t[3]	Compute DAG Shortest Beer Path\n";
+		cout << "\t[4]	Query Time\n";
+			cout << "\t\t[1]	Time Building DAG\n";
+			cout << "\t\t[2]	Time Computing Shortest Path\n";
+			cout << "\t\t[3]	Time Computing Shortest Beer Path\n";
+			cout << "\t\t[4]	Time Getting Instance of Shortest Path\n";
+			cout << "\t\t[5]	Time Getting Instance of Shortest Beer Path\n\n";
+
 		clock_t start;
 
 		int option;
@@ -45,9 +47,37 @@ void test::test_dag(graph& G){
 
 				src = G.get_vertex(s);
 				dest = G.get_vertex(d);
+				if (!src || !dest){
+					cout << "No vertex found...\n\n";
+					break;
+				}
 
 				cout << "Building DAG...\n";
+				D.reset();
 				D.build_dag(G, src, dest);
+				// for (auto v : D.mDAG){
+				// 	cout << v->data << endl;
+				// 	for (int i = 0; i < v->dag_edges.size(); i++){
+				// 		cout << "DEdge -> " << v->dag_edges[i].first->dest->data << " w: " << v->dag_edges[i].first->weight << endl;
+				// 		auto [apex, loc] = v->dag_edges[i].first->subpath;
+				// 		cout << "Apex: ";
+				// 		if (apex == NULL)
+				// 			cout << "NULL ";
+				// 		else 
+				// 			cout << apex->data;
+				// 		cout << "\t Loc: " << loc << endl;
+
+				// 		cout << "DBEdge -> " << v->dag_edges[i].second->dest->data << " w: " << v->dag_edges[i].second->weight << endl;
+				// 		auto [apexB, locB] = v->dag_edges[i].second->subpath;
+				// 		cout << "Apex BR: ";
+				// 		if (apexB == NULL)
+				// 			cout << "NULL ";
+				// 		else 
+				// 			cout << apexB->data;
+				// 		cout << "\t Loc BR: " << locB << endl;
+				// 	}
+				// 	cout << endl;
+				// }
 				cout << "Completed\n\n";
 				break;
 			}
@@ -57,6 +87,8 @@ void test::test_dag(graph& G){
 				cout << "Computing Shortest Path...\n";
 				D.shortest_path_dag();
 				cout << "Completed\n\n";
+				// for (int i = 1; i < D.mDAG.size(); ++i)
+				// 	printf("\u03B4(%d) = %f\tpred: %d\n", D.mDAG[i]->data, D.mDAG[i]->dist, D.mDAG[i]->pred->data);
 
 				cout << "Printing Shortest Path...\n";
 				printf("\u03B4(%d, %d) = %f\n", dcel::data(src), dcel::data(dest), dcel::dist(dest));
@@ -75,16 +107,31 @@ void test::test_dag(graph& G){
 				cout << "Computing Shortest Beer Path...\n";
 				D.shortest_beer_path_dag();
 				cout << "Completed\n\n";
+				// for (int i = 1; i < D.mDAG.size(); ++i)
+				// 	printf("\u03B4B(%d) = %f\tpred: (%d, %d)\n", D.mDAG[i]->data, D.mDAG[i]->distB, 
+				// 												 (D.mDAG[i]->predB.first) ? D.mDAG[i]->predB.first->data : -13,
+				// 												 D.mDAG[i]->predB.second);
 				
 				cout << "Printing Shortest Beer Path...\n";
-				printf("\u03B4(%d, %d) = %f\n", dcel::data(src), dcel::data(dest), dcel::distB(dest));
+				printf("\u03B4B(%d, %d) = %f\n", dcel::data(src), dcel::data(dest), dcel::distB(dest));
+				if (dcel::distB(dest) == INF){
+					cout << "There Exists No Beer Path...\n";
+					break;
+				}
+
 				start = clock();
 				vector<struct vertex*> pathB = D.print_spsbp_dag(dest);
 				D.get_spsbp_duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 
 				cout << "PATH: ";
-				for (auto v : pathB) cout << dcel::data(v) << " "; cout << "\n";
-				cout << "Completed\n\n";
+				for (auto v : pathB){ 
+					cout << dcel::data(v);
+					if (dcel::beer(v))
+						cout << "(BR)";
+					cout << " ";
+				}
+
+				cout << "\nCompleted\n\n";
 				break;
 			}
 
@@ -115,7 +162,7 @@ void test::test_dag(graph& G){
 
 			default:
 				D.reset();
-				exit(1);
+				// exit(1);
 				break; 
 		}
 	}
